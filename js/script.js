55fb125d2471f946a27a6f73e8d297e5fb090ae9
Parvez <103523874+khanparvez7744@users.noreply.github.com>
2022-08-19 12:26:49 +0530

    commit3
    commit3-desc

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
+});
